96f38c08e4096dcbef49d98ba9feed79824417e9
GSM <25423602+historybro@users.noreply.github.com>
2019-06-14 18:22:07 -0500

    added buttons maye?

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_0tlnpxgc:testing123@ds133187.mlab.com:33187/heroku_0tlnpxgc" || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
-app.get("/scrape", function (req, res) {
+app.get("/scrapearticle", function (req, res) {
   axios.get("https://www.coindesk.com/").then(function (response) {
     var $ = cheerio.load(response.data);
 
     $(".stream-article").each(function (i, element) {
-      var article = {};
-      
+      var article = {};      
       article.title = $(this)
       .attr("title");
       article.link = $(this)
@@ -39,33 +38,100 @@ app.get("/scrape", function (req, res) {
           console.log(err);
         });
       });
+    res.send("scrape complete");
+  });
+});
 
-    // $(".sidebar-price-widget-v2-list-item").each(function (i, element) {
-    //   var price = {};
-    //   price.title = $(this)
-    //     .children(".sidebar-price-widget-v2-list-item__meta")
-    //     .children(".sidebar-price-widget-v2-list-item__name")
-    //     .text();
-    //   price.price = $(this)
-    //     .children(".sidebar-price-widget-v2-list-item__data")
-    //     .children(".sidebar-price-widget-v2-list-item__price")
-    //     .text();
-    //   price.change = $(this)
-    //     .children(".sidebar-price-widget-v2-list-item__data")
-    //     .children(".sidebar-price-widget-v2-list-item__percent sidebar-price-widget-v2-list-item__text--down")
-    //     .text();
-    //   db.Price.create(price)
-    //   .then(function (dbArticle) {
-    //     console.log(dbArticle);
-    //   })
-    //   .catch(function (err) {
-    //     console.log(err);
-    //   });
-    // });
+app.get("/scrapeprice", function (req, res) {
+  axios.get("https://www.coindesk.com/").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    $(".sidebar-price-widget-v2-list-item").each(function (i, element) {
+      var price = {};
+      price.title = $(this)
+        .children(".sidebar-price-widget-v2-list-item__meta")
+        .children(".sidebar-price-widget-v2-list-item__name")
+        .text();
+      price.price = $(this)
+        .children(".sidebar-price-widget-v2-list-item__data")
+        .children(".sidebar-price-widget-v2-list-item__price")
+        .text();
+      price.change = $(this)
+        .children(".sidebar-price-widget-v2-list-item__data")
+        .children(".sidebar-price-widget-v2-list-item__percent sidebar-price-widget-v2-list-item__text--down")
+        .text();
+      db.Price.create(price)
+      .then(function (dbArticle) {
+        console.log(dbArticle);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    });
 
     res.send("scrape complete");
   });
 });
+
+$(document).on("click", "#artbtn", function () {
+  axios.get("https://www.coindesk.com/").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    $(".stream-article").each(function (i, element) {
+      var article = {};      
+      article.title = $(this)
+      .attr("title");
+      article.link = $(this)
+      .attr("href");
+      article.image = $(this)
+        .children(".image")
+        .children("img")
+        .attr("src");
+      article.summary = $(this)
+      .children(".meta")
+        .children("p")
+        .text();               
+        db.Article.create(article)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+      });
+    res.send("scrape complete");
+});
+
+$(document).on("click", "#pricebtn", function () {
+  axios.get("https://www.coindesk.com/").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    $(".sidebar-price-widget-v2-list-item").each(function (i, element) {
+      var price = {};
+      price.title = $(this)
+        .children(".sidebar-price-widget-v2-list-item__meta")
+        .children(".sidebar-price-widget-v2-list-item__name")
+        .text();
+      price.price = $(this)
+        .children(".sidebar-price-widget-v2-list-item__data")
+        .children(".sidebar-price-widget-v2-list-item__price")
+        .text();
+      price.change = $(this)
+        .children(".sidebar-price-widget-v2-list-item__data")
+        .children(".sidebar-price-widget-v2-list-item__percent sidebar-price-widget-v2-list-item__text--down")
+        .text();
+      db.Price.create(price)
+      .then(function (dbArticle) {
+        console.log(dbArticle);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    });
+
+    res.send("scrape complete");
+});
+
 
 app.get("/articles", function (req, res) {
   db.Article.find({})
